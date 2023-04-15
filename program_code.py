1ef50136c257b447ed0349c2935d11d47bb9806e
Ms Lawliet <alexieanoya@gmail.com>
2023-04-15 20:54:49 +0800

    made a dict for letter-to-number translation :)

--- program_code.py
+++ program_code.py
@@ -14,7 +14,12 @@ class Cipher:
         self.message = message
         self.key = key
 
-# translate message and key to number values
+    def produce_cipher(self):           # translate message and key to number values
+        # create dict for letters and corresponding values
+        translation = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9, 'K': 10,
+                       'L': 11, 'M': 12, 'N': 13, 'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18, 'T': 19, 'U': 20,
+                       'V': 21, 'W': 22, 'X': 23, 'Y': 24, 'Z': 25}
+
 # write values of message in one row
 # write values of keyword until no. of values in message == no. of values in keyword
 # add values of message and keyword with same index
