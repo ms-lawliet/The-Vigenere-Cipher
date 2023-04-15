80123bef12e5702a60bdf956e806305888448e93
Ms Lawliet <alexieanoya@gmail.com>
2023-04-15 20:19:20 +0800

    created pseudocode for program flow :)

--- program_code.py
+++ program_code.py
@@ -1,3 +1,12 @@
 # Write a program that asks the user for the plaintext (all uppercase letters,
 # no spaces) and the keyword (all uppercase letters) and produce the
 # ciphertext using the Vigenère cipher.
+
+# take in message and keyword from user (capital letters, no space)
+# translate message and key to number values
+# write values of message in one row
+# write values of keyword until no. of values in message == no. of values in keyword
+# add values of message and keyword with same index
+# for values >= 26, convert to corresponding values by starting again at index 0
+# convert values to corresponding letters
+# print final result
